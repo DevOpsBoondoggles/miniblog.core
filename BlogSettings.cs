4da8b464a384f74ed487402b96c7812db98930ff
Gabriel McColl <gabriel@cloudconfusion.co.uk>
2018-02-07 20:57:35 +0000

    made some changes

--- BlogSettings.cs
+++ BlogSettings.cs
@@ -2,9 +2,9 @@
 {
     public class BlogSettings
     {
-        public string Name { get; set; } = "miniblog";
-        public string Description { get; set; } = "A short description of the blog";
-        public string Owner { get; set; } = "The Owner";
+        public string Name { get; set; } = "Cloud Confusion";
+        public string Description { get; set; } = "Learning some of that Cloud stuff";
+        public string Owner { get; set; } = "Gabriel McColl";
         public int PostsPerPage { get; set; } = 2;
         public int CommentsCloseAfterDays { get; set; } = 10;
     }
